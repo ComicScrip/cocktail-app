732fca84602b7a8895274aeec2e5c14ce6b03f15
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-07-25 23:49:43 +0200

    refacto

--- screens/CocktailDetails.tsx
+++ screens/CocktailDetails.tsx
@@ -19,85 +19,6 @@ import Tag from '../components/Tag';
 import Ingredient from '../components/Ingredient';
 import { StackNavigationProp } from '@react-navigation/stack';
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    paddingBottom: 100,
-  },
-
-  cocktailImage: {
-    width: '100%',
-    height: 420,
-  },
-
-  ingredientsContainer: {
-    marginTop: 40,
-    marginBottom: 50,
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-  },
-  instructions: {
-    maxWidth: '85%',
-    marginTop: 30,
-    fontSize: 25,
-    textAlign: 'center',
-  },
-  step: {
-    display: 'flex',
-    flexDirection: 'row',
-    width: '85%',
-    alignItems: 'center',
-    marginBottom: 35,
-  },
-  stepNumber: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 60,
-    width: 60,
-    borderRadius: 30,
-    backgroundColor: 'orange',
-    marginRight: 30,
-  },
-  stepText: {
-    flex: 5,
-  },
-  stepNumberText: {
-    fontSize: 40,
-    opacity: 0.8,
-  },
-  instruction: {
-    fontSize: 20,
-  },
-  imageContainer: {
-    backgroundColor: '#222',
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    ...Platform.select({
-      ios: {
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.5,
-        shadowRadius: 10,
-      },
-      android: {
-        elevation: 5,
-      },
-    }),
-  },
-  favorite: {
-    margin: 50,
-  },
-  tagList: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-});
-
 export default function CocktailsDetails({
   route,
   navigation,
@@ -173,3 +94,80 @@ export default function CocktailsDetails({
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingBottom: 100,
+  },
+  cocktailImage: {
+    width: '100%',
+    height: 420,
+  },
+  ingredientsContainer: {
+    marginTop: 40,
+    marginBottom: 50,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  instructions: {
+    maxWidth: '85%',
+    marginTop: 30,
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  step: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '85%',
+    alignItems: 'center',
+    marginBottom: 35,
+  },
+  stepNumber: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    backgroundColor: 'orange',
+    marginRight: 30,
+  },
+  stepText: {
+    flex: 5,
+  },
+  stepNumberText: {
+    fontSize: 40,
+    opacity: 0.8,
+  },
+  instruction: {
+    fontSize: 20,
+  },
+  imageContainer: {
+    backgroundColor: '#222',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+  favorite: {
+    margin: 50,
+  },
+  tagList: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+});
